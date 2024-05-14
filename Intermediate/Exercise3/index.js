73bc117f831bdafae0bdfa6d9fa68008a101de94
JarrodJackson2000 <jarrodjackson2000@hotmail.com>
2024-05-14 18:03:27 +1200

    added exercise 3

--- Intermediate/Exercise3/index.js
+++ Intermediate/Exercise3/index.js
@@ -1,68 +1,46 @@
-/* 5. Decimal number operations in JavaScript can lead to unexpected results, as in the
-following:
+/* Use the following animals array for the below tasks. Test each one by printing the result to
+the console. Review the following link for tips:
 
-let twentyCents = 0.20
-let tenCents = 0.10
-console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`)
-// 0.2 + 0.1 = 0.30000000000000004
+https://developer.mozilla.org/en-
+US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-We can sometimes avoid this using the toFixed function to force the number of decimal
-places as below, but it’s not always useful:
+a) Add 2 new values to the end
+b) Add 2 new values to the beginning
+c) Sort the values alphabetically
+d) Write a function replaceMiddleAnimal(newValue) that replaces the value in the
+middle of the animals array with newValue
+e) Write a function findMatchingAnimals(beginsWith) that returns a new array
+containing all the animals that begin with the beginsWith string. Try to make it work
+regardless of upper/lower case. */
 
-let fixedTwenty = twentyCents.toFixed(2);
-let fixedTen = tenCents.toFixed(2);
-console.log(fixedTwenty + fixedTen) //why is this not working? */
+const animals = ["Tiger", "Giraffe", "Gazelle", "Gator"];
+console.log(animals);
 
-let twentyCents = 0.2;
-let tenCents = 0.1;
-console.log(`${twentyCents} + ${tenCents} = ${twentyCents + tenCents}`);
-// 0.2 + 0.1 = 0.30000000000000004
+animals.push("Lion", "Hippo");
 
-let fixedTwenty = twentyCents.toFixed(2);
-let fixedTen = tenCents.toFixed(2);
-console.log(fixedTwenty + fixedTen); //why is this not working? This does not work because the + operator is concatenating the strings
+console.log(animals);
 
-/* a) Explain why the above code returns the wrong answer
-b) Create a function currencyAddition(float1, float2) which safely adds the two
-decimal numbers float1 and float2 and returns the correct float result.
-c) Create a function currencyOperation(float1, float2, operation) which
-safely performs the given operation (either +, -, / or *) on the two numbers and returns
-the correct float result. https://developer.mozilla.org/en-
-US/docs/Web/JavaScript/Reference/Statements/switch may be useful.
+animals.unshift("Rhino", "Cheetah");
 
-d) (Extension) Extend the above function to include a fourth argument numDecimals
-which allows the operation to support different amounts of decimal places from 1 to 10. */
+console.log(animals);
 
-let operation = "*";
+animals.sort((array1, array2) => (array1 > array2 ? 1 : -1));
 
-let numDecimals = 5;
-/* function currencyAddition(a, b) {
-  let correctAddition = parseFloat(a) + parseFloat(b);
-  return correctAddition.toFixed(1);
-} */
-console.log(calculate(twentyCents, tenCents, operation, numDecimals));
+console.log(animals.sort());
 
-function calculate(a, b, operation, numDecimals) {
-  const operator = operation;
-  switch (operation) {
-    case "-":
-      let correctSubtraction = parseFloat(a) - parseFloat(b);
-      return correctSubtraction.toFixed(numDecimals);
-      break;
-
-    case "+":
-      let correctAddition = parseFloat(a) + parseFloat(b);
-      return correctAddition.toFixed(numDecimals);
-      break;
-
-    case "/":
-      let correctMultiplication = parseFloat(a) / parseFloat(b);
-      return correctMultiplication.toFixed(numDecimals);
-      break;
-
-    case "*":
-      let correctDivision = parseFloat(a) * parseFloat(b);
-      return correctDivision.toFixed(numDecimals);
-      break;
-  }
+function replaceMiddleAnimal(newValue) {
+  animals.splice(2, 1, newValue);
 }
+
+//replaceMiddleAnimal("Elephant");
+
+console.log(animals);
+
+function findMatchingAnimals(beginsWith) {
+  const matchingAnimals = animals.filter(function (animals) {
+    return animals.toUpperCase().startsWith(beginsWith.toUpperCase());
+  });
+  return matchingAnimals;
+}
+
+console.log(findMatchingAnimals("L"));
