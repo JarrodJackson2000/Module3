262f88d0f617a23e30b5c14e5b2aed00645a609d
JarrodJackson2000 <jarrodjackson2000@hotmail.com>
2024-05-16 18:16:14 +1200

    added exercise 6

---
+++ Advanced/Exercise6/index.js
@@ -0,0 +1,24 @@
+/* Use the Function prototype to add a new delay(ms) function to all functions, which can
+be used to delay the call to that function by ms milliseconds. 
+a) Use the example multiply function below to test it with, as above, and assume that all
+delayed functions will take two parameters
+b) Use apply to improve your solution so that delayed functions can take any number of
+parameters
+c) Modify multiply to take 4 parameters and multiply all of them, and test that your
+delay prototype function still works. */
+
+function multiply(a, b, c, d) {
+  console.log(a * b * c * d);
+}
+//multiply.delay(500)(5, 5); // prints 25 after 500 milliseconds
+
+Function.prototype.delay = function (ms) {
+  const func = this;
+  return function (...args) {
+    setTimeout(() => {
+      func(...args);
+    }, ms);
+  };
+};
+
+multiply.delay(2000)(5, 5, 2, 2);
